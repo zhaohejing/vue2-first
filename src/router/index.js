802e27291dc46e9b5ea66c35b6ea6a988228f5f1
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-08-01 16:49:54 +0800

    简单修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,85 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import api from './../fetch/api';
+import Vue from "vue";
+import Router from "vue-router";
+//import api from "./../fetch/api";
 
 Vue.use(Router);
-let menus=[];
-api.mineDashboard().then(r=>{
-        if(r.success){
-            menus=r.result;
-        }
-});
+// let menus = [];
+// api.mineDashboard().then(r => {
+//   if (r.success) {
+//     menus = r.result;
+//   }
+// });
 export default new Router({
-    routes: [{
-            path: '/',
-            redirect: '/login'
+  routes: [
+    {
+      path: "/",
+      redirect: "/login"
+    },
+
+    {
+      path: "/dashboard",
+      component: resolve =>
+        require(["../components/common/index.vue"], resolve),
+      children: [
+        {
+          path: "",
+          component: resolve =>
+            require(["../components/common/dashboard.vue"], resolve)
         },
         {
-            path: '/dashboard',
-            component: resolve => require(['../components/common/index.vue'], resolve),
-            children: [{
-                    path: '',
-                    component: resolve => require(['../components/common/dashboard.vue'], resolve)
-                },
-                {
-                    path: '/form',
-                    component: resolve => require(['../components/page/form.vue'], resolve)
-                },
-                {
-                    path: '/table',
-                    component: resolve => require(['../components/page/table.vue'], resolve)
-                },
-                {
-                    path: '/markdown-viewer',
-                    component: resolve => require(['../components/page/markdown-viewer.vue'], resolve)
-                },
-                {
-                    path: '/markdown-editor-1',
-                    component: resolve => require(['../components/page/markdown-editor-1.vue'], resolve)
-                },
-                {
-                    path: '/markdown-editor-2',
-                    component: resolve => require(['../components/page/markdown-editor-2.vue'], resolve)
-                },
-                {
-                    path: '/rtf',
-                    component: resolve => require(['../components/page/rtf.vue'], resolve)
-                },{
-                    path: '/upload',
-                    component: resolve => require(['../components/page/upload.vue'], resolve)
-                },{
-                    path: '/echarts',
-                    component: resolve => require(['../components/page/echarts.vue'], resolve)
-                }
-            ]
+          path: "/figure",
+          component: resolve =>
+            require(["../components/page/store/index.vue"], resolve)
         },
         {
-            path: '/login',
-            component: resolve => require(['../components/common/login.vue'], resolve)
+          path: "/integral",
+          component: resolve =>
+            require(["../components/page/integral/index.vue"], resolve)
         },
-    ]
-})
+        {
+          path: "/form",
+          component: resolve =>
+            require(["../components/page/form.vue"], resolve)
+        },
+        {
+          path: "/table",
+          component: resolve =>
+            require(["../components/page/table.vue"], resolve)
+        },
+        {
+          path: "/markdown-viewer",
+          component: resolve =>
+            require(["../components/page/markdown-viewer.vue"], resolve)
+        },
+        {
+          path: "/markdown-editor-1",
+          component: resolve =>
+            require(["../components/page/markdown-editor-1.vue"], resolve)
+        },
+        {
+          path: "/markdown-editor-2",
+          component: resolve =>
+            require(["../components/page/markdown-editor-2.vue"], resolve)
+        },
+        {
+          path: "/rtf",
+          component: resolve => require(["../components/page/rtf.vue"], resolve)
+        },
+        {
+          path: "/upload",
+          component: resolve =>
+            require(["../components/page/upload.vue"], resolve)
+        },
+        {
+          path: "/echarts",
+          component: resolve =>
+            require(["../components/page/echarts.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/login",
+      component: resolve => require(["../components/common/login.vue"], resolve)
+    }
+  ]
+});
