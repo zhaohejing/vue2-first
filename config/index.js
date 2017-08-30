3b3656bfacaf0dc9e05f85e6d66f8181a9c123ae
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-08-30 09:29:06 +0800

    www

--- config/index.js
+++ config/index.js
@@ -27,7 +27,17 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target:'http://tcservice.hijigu.com:8080',
+        changeOrigin:true,
+        pathRewrith:{
+        '^/api':
+        '/api'
+        }
+        },
+
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
